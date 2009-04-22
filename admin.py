7123f6e93d1b506b85091ea06f6f7cea0ee40b37
= <nek4life@gmail.com>
2009-04-21 21:45:48 -0400

    Hid notes_html from the admin

--- admin.py
+++ admin.py
@@ -3,6 +3,6 @@ from djangolicious.models import Bookmark
 
 class BookmarkAdmin(admin.ModelAdmin):
     prepopulated_fields = { 'slug': ('title',) }
-    exclude = ('post_hash', 'post_meta', 'save_date', 'queued')
+    exclude = ('post_hash', 'post_meta', 'save_date', 'queued', 'notes_html')
 
 admin.site.register(Bookmark, BookmarkAdmin)
